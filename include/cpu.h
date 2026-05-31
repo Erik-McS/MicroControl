8e591c0bf5f05db34c5f95172675de4efa3ab96c
Erik McSeveney <2911240m@student.gla.ac.uk>
2026-05-31 21:59:12 +0100

    - Built dynamic file I/O to load raw binary cartridges (`.bin`) into virtual RAM via CLI. - Added graceful error handling for missing files to prevent segmentation faults. - Expanded ISA with LDB (Load Register B) and ADD operations. - Validated ALU logic with successful register addition test. - TODO: Implement control flow (JMP/OUT) and infinite loop architecture.

---
+++ include/cpu.h
@@ -0,0 +1,30 @@
+#ifndef CPU_H
+# define CPU_H
+
+#include <stdint.h>
+
+#define MAX_RAM_SIZE 256
+#define DEBUG 1
+
+
+typedef enum Instructions{
+    INS_NOP = 0x00,
+    INS_LDA = 0x01,
+    INS_HLT = 0xFF,
+    INS_LDB = 0x02,
+    INS_ADD = 0x03
+}Instructions;
+
+/* Defines the cpu*/
+typedef struct cpu
+{
+    /* Array for ram */
+    uint8_t ram[MAX_RAM_SIZE];
+    /* Registers*/
+    uint8_t reg_a;
+    uint8_t reg_b;
+    /* PC */
+    uint8_t program_counter;
+}cpu;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,68 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "cpu.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 
+    /* testing correct argc size*/
+    if (argc != 2){
+        printf("Wrong number of arguments\n");
+        return 1;
+    }
+
+    cpu micro_cpu = {0};
+    bool is_running = true;
+    FILE* rom;
+    rom = fopen(argv[1],"rb");
     
+    if(rom == NULL){printf("Error while opening the file\n"); return 1;}
+
+    fread(micro_cpu.ram,1,MAX_RAM_SIZE,rom);
+    fclose(rom);
+
+    /* cpu loop*/
+    while (is_running){
+        /*get the instruction*/
+        uint8_t current_instruction = micro_cpu.ram[micro_cpu.program_counter];
+
+        switch(current_instruction){
+            /* 2 bytes instruction*/
+            case INS_LDA:
+                /* move to the value to load in reg_a and load it*/
+                micro_cpu.program_counter++;
+                micro_cpu.reg_a = micro_cpu.ram[micro_cpu.program_counter];
+                micro_cpu.program_counter++;
+                break;
+            case INS_LDB:
+                /* move to the value to load in reg_a and load it*/
+                micro_cpu.program_counter++;
+                micro_cpu.reg_b = micro_cpu.ram[micro_cpu.program_counter];
+                micro_cpu.program_counter++;
+                break;
+            case INS_NOP:
+                /* do nothing*/
+                micro_cpu.program_counter++;
+                break;
+            case INS_ADD:
+                micro_cpu.reg_a = micro_cpu.reg_a + micro_cpu.reg_b;
+                micro_cpu.program_counter++;
+                break;
+            case INS_HLT:
+                /* exit*/
+                is_running = false;
+                break;
+            default:
+                printf("Unknown command, exiting\n");
+                is_running = false;
+                break;
+        }
+    }
+    #if DEBUG
+        printf("memory at index 0: %02X \n",micro_cpu.ram[0]);
+        printf("memory at index 1: %02X \n",micro_cpu.ram[1]);
+        printf("memory at index 2: %02X \n",micro_cpu.ram[2]);
+    #endif
+    printf("Value in reg_a: %02X \n", micro_cpu.reg_a);
+    return 0;
 }
 
