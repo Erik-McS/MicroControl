a5b48fe043b55fa9305142b56afe13c54cd3ad2b
Erik McSeveney <2911240m@student.gla.ac.uk>
2026-06-01 10:36:23 +0100

    edited the swith in alu, logic error

--- src/alu.c
+++ src/alu.c
@@ -8,7 +8,6 @@ uint8_t alu_compute(uint8_t operation,uint8_t operand_a, uint8_t operand_b){
     {
     case ALU_ADD:
         return operand_a + operand_b;
-        break;
     default:
         return 0;
         break;
